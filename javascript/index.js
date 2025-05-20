2b762ac38b45c47564fe65a00db085852a16ba69
mariatanya <151530541+mariatanya@users.noreply.github.com>
2025-05-20 04:23:02 +0200

    Added the javascript logic for the dropdown cities

--- javascript/index.js
+++ javascript/index.js
@@ -21,3 +21,48 @@ function homePageTime() {
 
 homePageTime();
 setInterval(homePageTime, 60000);
+
+function updateCityTime(event) {
+  let timezones = {
+    "Australia/Brisbane": "Brisbane",
+    "America/Argentina/Buenos_Aires": "Buenos Aires",
+    "Africa/Casablanca": "Casablanca",
+    "Asia/Kolkata": "Chennai",
+    "Asia/Dubai": "Dubai",
+    "Africa/Harare": "Harare",
+    "Asia/Hong_Kong": "Hong Kong",
+    "Asia/Manila": "Manila",
+    "Europe/Prague": "Prague",
+    "America/Sao_Paulo": "São Paulo",
+    "America/Vancouver": "Vancouver",
+  };
+
+  let timezone = event.target.value;
+  let tz, cityName;
+
+  if (timezone === "current") {
+    tz = moment.tz.guess();
+    cityName = tz.replace("_", "").split("/")[1];
+  } else {
+    tz = timezone;
+    cityName = timezones[timezone];
+  }
+
+  if (timezone.length > 0) {
+    let now = moment.tz(tz);
+    let date = now.format("dddd, D MMMM");
+    let time = now.format("HH:mm  ");
+    let zone = now.format("zz");
+
+    let cities = document.querySelector("#cities");
+    cities.innerHTML = `<div class="city">
+          <div class="city-info">
+            <h2>${cityName}</h2>
+            <div class="date">${date}</div>
+          </div>
+          <div class="time">${time}<small class="zone">${zone}</small></div>
+        </div>`;
+  }
+}
+let select = document.querySelector("#city");
+select.addEventListener("change", updateCityTime);
